70a2f05d5a4d27312c50452c640e3f56a5b96028
xiaowang <2805528068@qq.com>
2021-10-07 20:56:29 +0800

    todolist

--- React.md
+++ React.md
@@ -1428,3 +1428,6 @@ export default class index extends Component {
 ## (3).组件中函数传值
 - bind关键字，this关键字
 - <button onClick={this.函数名.bind(this,传递的值)}>删除</button>
+## (4).随机id用的库
+- UUID ---npm i uuid  迷你版：nanoid ---npm i nanoid  
+- 使用 nanoid()
--- React应用-基于React脚手架/my-react-app2/src/Todolist案例/Header/index.jsx
+++ React应用-基于React脚手架/my-react-app2/src/Todolist案例/Header/index.jsx
@@ -8,17 +8,25 @@ export default class index extends Component {
       alert(this.input1.value)
     }
   }  
-  add = ()=>{
-    const {todolist} = this.props
-    const a = {id:todolist.length+1, name:this.input1.value}
-    this.setState({todolist:[a,...todolist]})
-    this.props.liuyan1([a,...todolist])
+  add = (event)=>{
+    // .trim()---去掉空格
+    if (this.input1.value.trim() !== ''){
+      const {todolist} = this.props
+      const a = {id:todolist.length+1, name:this.input1.value}
+      this.setState({todolist:[a,...todolist]})
+      this.props.liuyan1([a,...todolist])
+    } else{
+      alert("输入不能为空")
+    }
+    //清空输入
+    this.input1.value =''
+
   }  
   render() {
     return (
       <div>
         <input type="text" ref={c=>this.input1=c} />
-        <span><button onClick = {this.add}>留言</button></span>
+        <span><button  onClick = {this.add}>留言</button></span>
       </div>
     )
   }
